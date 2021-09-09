c91a5df7a48e0cec090879b4715c60c6016f8a8e
erez <erezmizra@gmail.com>
2021-09-10 02:28:01 +0300

    echo PWD

--- staging/ci.py
+++ staging/ci.py
@@ -34,7 +34,8 @@ def git_api_comm():
         pureFolder = realFolder.replace("['","")
         
         print(pureFolder)
-        os.system("cd gan-shmuel/")
+        os.system("echo $PWD")
+        os.system("cd /gan-shmuel/")
         os.system("git checkout origin/" + branch)
         os.system("git checkout " + branch)
         os.system("git pull")
