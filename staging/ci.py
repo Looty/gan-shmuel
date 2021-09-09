2626214ae8eb7f7b32ce04f9af9f42d003df5016
erez <erezmizra@gmail.com>
2021-09-09 21:05:14 +0300

    added bash commands inside flask

--- staging/ci.py
+++ staging/ci.py
@@ -1,10 +1,11 @@
-from os import truncate
 from flask import Flask, request , json
+import subprocess
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def main():
     return "good"
+
 @app.route('/health',methods=["GET"])
 def health():
     return "ok"
@@ -13,6 +14,20 @@ def health():
 def git_api_comm():
     if request.headers['Content-Type'] == 'application/json':
         my_commit = request.json
+
+        branch_ref = my_commit.ref
+        directory_ref = my_commit.commits["modified"]
+        
+        branch = branch_ref.split("/")[2]
+        directory = directory_ref.split("/")[0]
+
+        subprocess.run(["echo hello"])
+        subprocess.run(["git checkout origin", branch], stderr=subprocess.PIPE, text=True)
+        subprocess.run(["git pull"], stderr=subprocess.PIPE, text=True)
+        subprocess.run(["pushd", directory], stderr=subprocess.PIPE, text=True)
+        subprocess.run(["docker-compose up"], stderr=subprocess.PIPE, text=True)
+        subprocess.run(["popd"], stderr=subprocess.PIPE, text=True)
+
         print(my_commit)
         return my_commit
 
