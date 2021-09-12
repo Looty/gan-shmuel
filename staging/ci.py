80062e7d0a5dadc3610cab09a27303bca179ce9d
ImEilon <eilon696@gmail.com>
2021-09-12 20:36:51 +0300

    Added Schedule Emailing + LogFile

--- staging/ci.py
+++ staging/ci.py
@@ -1,7 +1,12 @@
 import os
 from flask import Flask, request ,json
+import datetime
+from flask import Response
+from flask_mail import Mail, Message
+from flask_apscheduler import APScheduler
 
 app = Flask(__name__)
+scheduler = APScheduler()
 
 @app.route('/', methods=['GET'])
 def main():
@@ -26,12 +31,24 @@ def git_api_comm():
         branch = branch_ref.split("/")[2]
         print(branch)
         realDir = directory_ref["modified"]
-
-        dirToString = str(realDir)
+        realadded = directory_ref["added"]
+        if realDir:
+            dirToString = str(realDir)
+        else:
+            dirToString = str(realadded)
+        #dirToString = str(realDir)
         realFolder = dirToString.split("/")[0]
         pureFolder = realFolder.replace("['","")
         
         print(pureFolder)
+
+        date = datetime.datetime.now()
+        dt_string = date.strftime("%d/%m/%Y %H:%M:%S")
+        nm_dt = date.strftime("%d/%m/%Y_%H")
+        with open(f"./logFile_"+nm_dt+".txt", "a+") as com_log:
+            com_log.write("[{0}] {4} {1} {2} {3}".format(dt_string,branch,pureFolder,com_msg,author))
+            com_log.write("\n")
+
         os.chdir("/gan-shmuel/")
         os.system("ls -a")
         os.system("git checkout origin/" + branch)
@@ -46,5 +63,30 @@ def git_api_comm():
         return my_commit
 
 
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'autmailer101@gmail.com'
+app.config['MAIL_PASSWORD'] = '12341234!'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
+
+@app.route('/mailer')
+def maiLogger():
+    with app.app_context():
+        mail_list=["oronboy100@gmail.com","eilon696@gmail.com","ron981@gmail.com"]
+        for m in range(len(mail_list)):
+            msg = Message('LogFile Just For you Green Team Leaders!()', sender = 'autmailer101@gmail.com', recipients = [mail_list[m]])
+            msg.body = "Hello from Green Devops AutoMailer! \n Weve got new log for you (Attached)"
+            with app.open_resource("logFile_"+nm_dt+".txt"") as fp:
+                msg.attach("logFile_"+nm_dt+".txt"", "text/plain", fp.read()) 
+            mail.send(msg)
+            return "Sent!"
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    scheduler.add_job(id ='Scheduled task', func = maiLogger , trigger = 'interval', minutes = 720)
+    scheduler.start()
+    app.run(host='0.0.0.0')
