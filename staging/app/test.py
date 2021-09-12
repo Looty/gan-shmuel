830bcabccc3ebb52f880a5b59af4bccf3e08f945
erez <erezmizra@gmail.com>
2021-09-13 00:20:01 +0300

    added back sys.exit

--- staging/app/test.py
+++ staging/app/test.py
@@ -1,15 +1,18 @@
 import sys
+
 # ...
 # ...
 # ...
 # ...
+
 test = "OK"
 def tester():
     if test == "OK":
         #print("All test passed!")
-        return "OK"
+        sys.exit(0)
         #os.system("python rest.py")
     else:
         #print("A test failed! the server was not started!")
-        return "ERROR"
+        sys.exit(1)
+
 x = tester()
