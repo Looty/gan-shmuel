f9dd8c39ec969f307e63c4f06e7a42f9c74230a8
Oron Cohen <oronboy100@gmail.com>
2021-09-12 22:56:57 +0300

    working funcs: 	healthcheck 	provider POST&PUT 	Truck POST&PUT 	Rates POST&GET

--- staging/app/app/routes.py
+++ staging/app/app/routes.py
@@ -1,7 +1,21 @@
-from flask import render_template, request
+from flask import render_template, request, jsonify
 from app import app
 import mysql.connector
+import sys
 import json
+import os
+
+def init_db():
+    return mysql.connector.connect(
+        password='root', 
+        user='root', 
+        host='db', 
+        database='billdb')
+    
+def to_json(cursor):
+    headers = [col[0] for col in cursor.description]
+    rows = cursor.fetchall()
+    return jsonify(list(map(lambda row: dict(zip(headers,row)) ,rows)))
 
 
 @app.route('/')
@@ -13,14 +27,8 @@ def index():
 @app.route('/health', methods=['GET'])
 def health():
     try:
-        mydb = mysql.connector.connect(
-            password='root', 
-            user='root', 
-            host='db', 
-            database='billdb', 
-            )
-    
-        mycursor = mydb.cursor()
+        conn = init_db()
+        mycursor = conn.cursor()
         mycursor.execute("show tables")
         res = str(mycursor.fetchall())
     except:
@@ -29,21 +37,133 @@ def health():
         return render_template('health.html', msg=res), 200
  
 
-
-@app.route('/provider', methods = ['POST', 'GET'])
+@app.route('/provider', methods = ['POST'])
 def postProvide():
-    if request.method == 'POST':
-      id = request.form['id']
-      name = request.form['name']
-      with open('provider.json', 'r+') as f:
-          feeds = json.load(f)
-          feeds.append({"id":id, "name":name})
-          f.seek(0)
-          print(feeds)
-          json.dump(feeds, f, indent="")
-      return render_template('provider.html', msg=("provider id: %s added successfuly." %id))
+        
+    try:
+        conn = init_db()
+        mycursor2 = conn.cursor()
+        Pname = request.args.get("name")
+  
+        sql = """INSERT INTO Provider (name) VALUES (%s)"""
+        print (sql, file=sys.stderr)
+        val = ([Pname])
+        mycursor2.execute(sql, val)
+        
+    except Exception as inst:
+        print(type(inst),file=sys.stderr)    # the exception instance
+        print(inst.args,file=sys.stderr)     # arguments stored in .args
+        return "db error", 500
     else:
-      id = request.args.get('id')
-      with open('provider.json', 'r') as f:
-          feeds = json.load(f)
-      return render_template('provider.html', msg=json.dumps(feeds))
+        id = mycursor2.lastrowid
+        retjson = {"id":id}
+        return json.dumps(retjson, indent="")
+
+
+@app.route('/provider/<id>', methods = ['PUT'])
+def putProvide(id):    
+    try:
+        conn = init_db()
+        mycursor2 = conn.cursor()
+        Pname = request.args.get("name")
+        sql = """UPDATE Provider SET name = %s WHERE id = %s"""
+        print (sql, file=sys.stderr)
+        val = ([Pname, id])
+        mycursor2.execute(sql, val)
+     
+    except Exception as inst:
+        print(type(inst),file=sys.stderr)    # the exception instance
+        print(inst.args,file=sys.stderr)     # arguments stored in .args
+        return "db error", 500
+    else:
+        return "on id: "+id + ", updated Provider name to : "+Pname
+
+
+@app.route('/truck', methods = ['POST'])
+def postTrucks():
+        
+    try:
+        conn = init_db()
+        mycursor3 = conn.cursor()
+        Pid_in = request.args.get("id")
+        Provider_id_in = request.args.get("provider_id")
+        sql = """INSERT INTO Trucks (id, provider_id) VALUES (%s, %s)"""
+        print (sql, file=sys.stderr)
+        val = ([Pid_in, Provider_id_in])
+        mycursor3.execute(sql, val)
+        
+    except Exception as inst:
+        print(type(inst),file=sys.stderr)    # the exception instance
+        print(inst.args,file=sys.stderr)     # arguments stored in .args
+        return "db error", 500
+    else:
+        id = mycursor3.lastrowid
+        retjson = {"id":id}
+        return json.dumps(retjson, indent="")
+    
+
+@app.route('/truck/<id>', methods = ['PUT'])
+def putTrucks(id):    
+    try:
+        conn = init_db()
+        mycursor4 = conn.cursor()
+        provider_id = request.args.get("provider_id")
+        sql = """UPDATE Trucks SET provider_id = %s WHERE id = %s"""
+        print (sql, file=sys.stderr)
+        val = ([provider_id, id])
+        mycursor4.execute(sql, val)
+     
+    except Exception as inst:
+        print(type(inst),file=sys.stderr)    # the exception instance
+        print(inst.args,file=sys.stderr)     # arguments stored in .args
+        return "db error", 500
+    else:
+        return "on id: "+id + ", updated id to : "+provider_id 
+
+
+@app.route('/rates', methods = ['POST'])
+def postRates():
+    try:
+        conn = init_db()
+        mycursor5 = conn.cursor()
+        sql = """DELETE FROM Rates"""
+        print (sql, file=sys.stderr)
+        mycursor5.execute(sql)
+        
+        filename = request.args.get("file")
+        filename = os.path.join("..","in", filename)
+        rates = []
+        if os.path.isfile(filename):
+            with open (filename, "r" ) as exelfile:
+                lines = exelfile.readlines()
+                headers = lines[0][:-1].split(",")
+                for line in lines[1:]:
+                    row = dict()
+                    values = line[:-1].split(",")
+                    for i in range(len(headers)):
+                        row[headers[i]] = values[i] 
+                    rates.append(row)
+            
+            sql = """INSERT INTO Rates (product_id, rate, scope) VALUES (%s, %s, %s)"""
+            mycursor5.executemany(sql, map(lambda r: [r["Product"], r["Rate"], r["Scope"]], rates))
+            
+    except Exception as inst:
+        print(type(inst),file=sys.stderr)    # the exception instance
+        print(inst.args,file=sys.stderr)     # arguments stored in .args
+        return "db error", 500
+    else:
+        return "rates updated",200
+
+
+@app.route('/rates', methods=['GET'])
+def getRates():
+    try:
+        conn = init_db()
+        mycursor6 = conn.cursor()
+        mycursor6.execute("SELECT product_id,rate,scope FROM Rates")
+        return to_json(mycursor6)
+
+    except Exception as inst:
+        print(type(inst),file=sys.stderr)    # the exception instance
+        print(inst.args,file=sys.stderr)     # arguments stored in .args
+        return "db error", 500
--- staging/db/billingdb.sql
+++ staging/db/billingdb.sql
@@ -15,17 +15,19 @@ CREATE TABLE IF NOT EXISTS `Provider` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  AUTO_INCREMENT=10001 ;
+) ENGINE=MyISAM ;
 
 CREATE TABLE IF NOT EXISTS `Rates` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `product_id` varchar(50) NOT NULL,
   `rate` int(11) DEFAULT 0,
   `scope` varchar(50) DEFAULT NULL,
-  FOREIGN KEY (scope) REFERENCES `Provider`(`id`)
+  FOREIGN KEY (scope) REFERENCES `Provider`(`id`),
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM ;
 
 CREATE TABLE IF NOT EXISTS `Trucks` (
-  `id` varchar(10) NOT NULL,
+  `id` varchar(10) NOT NULL UNIQUE,
   `provider_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`provider_id`) REFERENCES `Provider`(`id`)
