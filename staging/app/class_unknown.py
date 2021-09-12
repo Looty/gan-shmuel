c8fe45b917ecf4d25dd87e1c013e4f44508044ac
ron981 <ron981@gmail.com>
2021-09-12 21:57:52 +0300

    Fixed First Ver

--- staging/app/class_unknown.py
+++ staging/app/class_unknown.py
@@ -4,8 +4,11 @@ def GET_unknown():
 	mySQL = mysql_db()
 	try:
 		print("DB error")
-		info = mySQL.getData("select 1;")		
-		return info
+		info = mySQL.getData("SELECT distinct id FROM containers WHERE weight IS NULL")		
+		return '\n'.join(map(str,info))
 		
 	except:		
 		return "Weight data is ok"
+
+
+
---
+++ staging/app/test.py
@@ -0,0 +1,16 @@
+import sys
+# ...
+# ...
+# ...
+# ...
+test = "OK"
+def tester():
+    if test == "OK":
+        #print("All test passed!")
+        return "OK"
+        #os.system("python rest.py")
+    else:
+        #print("A test failed! the server was not started!")
+        return "ERROR"
+x = tester()
+sys.exit("OK")
