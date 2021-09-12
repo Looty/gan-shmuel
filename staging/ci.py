124f590e10062fd77b11c9cad8b120eb9c3ec74b
erez <erezmizra@gmail.com>
2021-09-12 22:55:43 +0300

    [python3 app/test.py]

--- staging/ci.py
+++ staging/ci.py
@@ -64,7 +64,8 @@ def git_api_comm():
         if branch != "Devops":
             os.system("docker-compose --env-file ./config/.env.test up --detach --build")
             os.system('docker exec -it $(docker container ps --filter label=container=app --filter label=team=' + branch.lower() + ' --format "{{.ID}}") sh')
-            test_result = subprocess.check_output("[python3 test.py]", shell=True)
+            os.system('ls -alF')
+            test_result = subprocess.check_output("[python3 app/test.py]", shell=True)
             os.system('exit')
 
             str_stop = "docker stop $(docker container ps --filter label=team=" + branch.lower() + " --format '{{.ID}}')"
