48a61bc4c1bdb4294011d0e4edbf0f545d58a9d1
ron981 <ron981@gmail.com>
2021-09-12 13:27:58 +0300

    fixed bugs

--- staging/app/rest.py
+++ staging/app/rest.py
@@ -10,10 +10,11 @@ def index():
 def helth():
     conn = mysql.connect()
     cursor = conn.cursor(pymysql.cursors.DictCursor)
-    cursor.execute("SELECT 1")
+    cursor.execute("select 1;")
     rows = cursor.fetchall()
     resp = jsonify(rows)
     resp.status_code = 200
     return resp
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
+
