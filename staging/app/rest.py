542fb344de5c13c7c4058ed4cc341f62b8f5fed2
ron981 <ron981@gmail.com>
2021-09-13 16:01:13 +0300

    gl

--- staging/app/rest.py
+++ staging/app/rest.py
@@ -1,13 +1,17 @@
 import pymysql
 from app import app
-#from db import mysql
+from db import mysql
 from flask import jsonify
-import mysql.connector
-from mysql.connector import Error
 
 @app.route('/')
 def users():
-    return 'ok'
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    cursor.execute("SELECT id FROM containers")
+    rows = cursor.fetchall()
+    resp = jsonify(rows)
+    resp.status_code = 200
+    return resp
 
 
 @app.route("/health", methods=['GET'])
@@ -16,26 +20,13 @@ def health():
 
 @app.route("/unknown", methods=['GET'])
 def unknown():
-    return 'ok'
-
-@app.route('/weight', methods=['GET'])
-def weigtPost():
-    # def GET_unknown():
-    try:
-            connection = mysql.connector.connect(host='db',database='roytuts',user='root',password='123456')
-            if (connection.is_connected()):
-                SQL = "SELECT * FROM containers;"
-                cursor = connection.cursor()
-                cursor.execute(SQL)
-                result_list = cursor.fetchall()      #return sql result
-                return '\n'.join(map(str,result_list)) #is s list type, need to be a dict
-                fields_list = cursor.description   # sql key name
-                return ("fields result -->",type(fields_list))
-                #print("header--->",fields)
-                cursor.close()
-                connection.close()
-    except Error as e:
-        return "bbb"
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    cursor.execute("SELECT distinct id FROM containers WHERE weight IS NULL")
+    rows = cursor.fetchall()
+    resp = jsonify(rows)
+    resp.status_code = 200
+    return resp
 
 
 
