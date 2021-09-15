e260098649c4768e18fa930fe8705225562aa167
erez <erezmizra@gmail.com>
2021-09-15 15:14:12 +0300

    removed comments on INSERT INTO ... on billingdb.sql

--- billing/db/billingdb.sql
+++ billing/db/billingdb.sql
@@ -36,7 +36,6 @@ CREATE TABLE IF NOT EXISTS `Trucks` (
 -- Dumping data
 --
 
-/*
 INSERT INTO Provider (`name`) VALUES ('ALL'), ('pro1'),
 (3, 'pro2');
 
@@ -45,4 +44,3 @@ INSERT INTO Rates (`product_id`, `rate`, `scope`) VALUES ('1', 2, 'ALL'),
 
 INSERT INTO Trucks (`id`, `provider_id`) VALUES ('134-33-443', 2),
 ('222-33-111', 1);
-*/
